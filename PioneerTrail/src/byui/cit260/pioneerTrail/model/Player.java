24da193a3b31efb097409d554f27b31e7f09eae7
Ariel <geo15006@byui.edu>
2018-05-17 16:40:03 -0600

    did up to page 6 so far

---
+++ PioneerTrail/src/byui/cit260/pioneerTrail/model/Player.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.pioneerTrail.model;
+
+import java.io.Serializable;
+/**
+ *
+ * @author arielgeorge
+ */
+public class Player implements Serializable{ 
+    
+    //class instance variables
+    private String name;
+    private double bestTime;
+    
+}
