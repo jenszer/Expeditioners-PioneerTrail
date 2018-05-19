5eb3c7bb46ea6da4b390ba44a9ca2fabeb8b6a4d
Jacob Enszer <Jacob Enszer@Jacob-Enszer-LT>
2018-05-19 12:40:50 -0600

    Instructor suggested changes

--- PioneerTrail/src/pioneertrail/PioneerTrail.java
+++ PioneerTrail/src/pioneertrail/PioneerTrail.java
@@ -16,20 +16,29 @@ public class PioneerTrail {
     
     public static void main(String[] args) {
        Player playerOne = new Player();
+       Inventory inventoryItem = new Inventory();
     
         playerOne.setName("Brigham Young");
         playerOne.setHealth(100);
 
         String playerOneName = playerOne.getName();
-        int playerOneHealth = playerOne.getHealth();
-
-        String actorFatherName = Actor.Father.getName();
-        String actorFatherDescription = Actor.Father.getDescription();
-        
+        int playerOneHealth = playerOne.getHealth();   
+          
         System.out.println("Name = " + playerOneName +
                        ", Health = " + playerOneHealth);
+               
+        String actorFatherName = Actor.Father.getName();
+        String actorFatherDescription = Actor.Father.getDescription(); 
         
         System.out.println(actorFatherName + ", " + actorFatherDescription);
-       
+    
+        inventoryItem.setName("Food");
+        inventoryItem.setWeight(15);
+        
+        String inventoryItemName = inventoryItem.getName();
+        int inventoryWeight = inventoryItem.getWeight();
+        
+        System.out.println("Inventory: " + inventoryItemName + "; Weight: " +
+                        inventoryWeight);
     }
 }
