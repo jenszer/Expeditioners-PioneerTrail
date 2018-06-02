c418b03af9a5864fab798b14246c47a47407c15f
Jacob Enszer <Jacob Enszer@Jacob-Enszer-LT>
2018-06-02 12:24:21 -0600

    Jacob's individual assignment

--- PioneerTrail/src/pioneertrail/control/ObstacleControl.java
+++ PioneerTrail/src/pioneertrail/control/ObstacleControl.java
@@ -14,8 +14,7 @@ public class ObstacleControl {
     public static int calcMortality(int mortalityRate, int noFood, int noWater, int noRest){
         int resourceUse;
         int deathRate;
-        int death = 0;
-        
+                
         if (noWater < 1){ 
         return -1;
         }      
@@ -26,29 +25,15 @@ public class ObstacleControl {
         return -3;
         }
        
+        
         resourceUse = noRest + noFood + noWater;
         deathRate = mortalityRate / resourceUse;
-                      
-        /**if (sicknessType = "fever"){
-            mortalityRate = 2;
-            resourceUse = noRest + noFood;
-            deathRate = mortalityRate/resourceUse;
-        else (sicknessType = "hypothermia") 
-            mortalityRate = 3;
-            resourceUse = noFood + noRest;
-            deathRate = mortalityRate/resourceUse;
-        else (sicknessType = "snakeBite") 
-            mortalityRate = 4;
-            resourceUse = noWater + noRest;
-            deathRate = mortalityRate/resourceUse;
-        else (sicknessType = "brokenBone") 
-            mortalityRate = 5;
-            resourceUse = noRest + noWater;
-            deathRate = mortalityRate/resourceUse;**/
-        if(deathRate >= .5){
-            death = 1;
-            return death;
+        
+        if(deathRate < .5){
+        return 0;
         }
-       return death;
+        else{
+        return 1;
+      }
     }
 }
