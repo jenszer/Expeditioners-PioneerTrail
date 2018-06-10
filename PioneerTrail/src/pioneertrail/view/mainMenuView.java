100bb8b6f80fb9849254d8abd19d28e88e8454c4
Jacob Enszer <Jacob Enszer@Jacob-Enszer-LT>
2018-06-09 19:14:09 -0600

    Jacob's Team Changes

---
+++ PioneerTrail/src/pioneertrail/view/mainMenuView.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pioneertrail.view;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jacob Enszer
+ */
+class mainMenuView {
+
+    public mainMenuView() {
+    }
+    
+   public void display(){
+        boolean endOfView = false;
+        String [] inputs;
+        
+        do {
+        inputs = this.getInputs();
+        
+        if((inputs.length < 1) || inputs[0].equalsIgnoreCase("Q")) {
+        break;
+        }
+        else{     
+        endOfView = doAction(inputs);
+        }
+        }
+        while (endOfView != true);
+        return;
+        }
+   
+    public String[] getInputs() {
+        String[] inputs = new String [1];
+        System.out.println("USER INPUT PROMPT");
+        
+        boolean valid = false;
+        while (valid == false){
+           System.out.println("Please enter a value: ");
+           Scanner userInput = new Scanner(System.in);
+           inputs[0] = userInput.nextLine();
+                                                       
+           if(inputs[0].trim().equals("") == true){
+           System.out.println("You must enter a non-blank value");
+           continue;
+           }           
+                      
+           valid = true;
+        }
+        return inputs;
+    }
+    public boolean doAction(String[] inputs) {
+        String menuItem = inputs[0];
+        menuItem = inputs[0].toUpperCase();
+        
+       switch (menuItem){
+            case "N": //for new game
+                //this.startNewGame();
+                System.out.println("N called");
+                break;
+            case "R":
+                //this.restartGame();
+                System.out.println("R called");
+                break;
+            case "H":
+                //this.getHelp();
+                System.out.println("H called");
+                break;
+            case "E":
+                System.out.println("E called");
+                break;   
+            default:
+            System.out.println("\nInvalid Menu Item.");
+                break;
+        }
+       return false;
+    }
+
+}
