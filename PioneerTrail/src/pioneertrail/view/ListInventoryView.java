9eb5b543337a766e1f1bae7cbd88e630717ce673
Jacob Enszer <Jacob Enszer@Jacob-Enszer-LT>
2018-06-25 22:42:23 -0600

    Bro Jones Suggested changes 6-25

--- PioneerTrail/src/pioneertrail/view/ListInventoryView.java
+++ PioneerTrail/src/pioneertrail/view/ListInventoryView.java
@@ -17,46 +17,10 @@ public class ListInventoryView extends View {
 
     private final ArrayList<Inventory> inventories = new ArrayList<>();
 
-    private final int resources = (int) (Math.random() * 8);
-
-    public ListInventoryView() {
-        super("\n H - Hunt"
-                + "\n V - View Inventory"
-                + "\n Q - Exit to Previous Menu");
-    }
-
     @Override
     public boolean doAction(String inputs) {
-        String menuItem = inputs;
-        menuItem = inputs.toUpperCase();
-
-        switch (menuItem) {
-            case "H":
-                this.huntResources();
-                break;
-            case "V":
-                this.viewInventory();
-                break;
-            default:
-                System.out.println("\nInvalid Menu Item.");
-                break;
-        }
-        return false;
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public int huntResources() {
-        String inputs = this.getInput("\nHow Many Bullets Would You"
-                + "Like to Use?\n");
-        int noBullets = Integer.parseInt(inputs);
-        int weight = HarvestHuntControl.calcHuntForResources(resources, noBullets);
-        System.out.println("You gathered " + weight + "lbs of resources");
-        return weight;
-
-    }
-
-    public void viewInventory() {
-        Inventory inventory = new Inventory();
-        System.out.println(inventory.toString());
-    }
 
 }
