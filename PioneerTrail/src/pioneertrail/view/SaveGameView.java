8691924e3204e4883b2307edcda759b9e748a7d0
jenszer <39323360+jenszer@users.noreply.github.com>
2018-06-29 16:11:47 +0000

    Create SaveGameView.java

---
+++ PioneerTrail/src/pioneertrail/view/SaveGameView.java
@@ -0,0 +1,34 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pioneertrail.view;
+
+/**
+ *
+ * @author arielgeorge
+ */
+public class SaveGame extends View {
+    
+    public void display(){
+        
+        System.out.println("Would You Like to Save Your Game?");
+    }
+
+    @Override
+    public boolean doAction(String inputs) {
+        String location = inputs[0].toUpperCase().trim();
+        switch (location){
+            case "Y": 
+                System.out.println("Game Saved");    
+                break;
+            case "N":
+                System.out.println("Game not Saved");
+    }
+                             
+        return true;
+    }
+    
+}
