f6b15963a50eddb5be2fa398985900a3b4fca0c0
Ariel <geo15006@byui.edu>
2018-07-21 23:16:07 -0600

    Ariel's individual assignment

--- PioneerTrail/src/pioneertrail/view/PrintInventoryView.java
+++ PioneerTrail/src/pioneertrail/view/PrintInventoryView.java
@@ -1,36 +1,55 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package pioneertrail.view;
-//
-//import java.io.FileWriter;
-//import java.io.IOException;
-//import java.util.logging.Level;
-//import java.util.logging.Logger;
-//
-///**
-// *
-// * @author arielgeorge
-// */
-//public class PrintInventoryView {
-//    FileWriter outFile = null;
-//    
-//    String fileLocation = "file:///Users/arielgeorge/Desktop/CIT260/inventoryItems.txt";
-//            
-//    try (FileWriter out = new FileWriter(filePath)) {
-//    
-//    outfile.write("Water\n");
-//    outfile.write("Food\n");
-//    outfile.write("Bullets\n");
-//    outfile.write("Hammer\n");
-//    outfile.write("Wheels\n");
-//    outfile.write("Rest\n");
-//    
-//    outFile.flush();
-//           
-//}  catch (IOException ex) {
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pioneertrail.view;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author arielgeorge
+ */
+
+public class PrintInventoryView {
+    
+    public void PrintInventoryView() {
+    
+        FileWriter outFile = null;
+    
+    String fileLocation  = "inventoryItems.txt";
+    
+ 
+    try{
+    
+        outFile = new FileWriter(fileLocation);
+            
+        outFile.write("Water\n");
+        outFile.write("Food\n");
+        outFile.write("Bullets\n");
+        outFile.write("Hammer\n");
+        outFile.write("Wheels\n");
+        outFile.write("Rest\n");
+            
+        outFile.flush();
+            
+        } catch (IOException ex){
+            System.out.println("Hello World");
+        }
+    }
+    
+    
+    
+            
+
+           
+//    } catch (IOException ex) {
 //    ErrorView.display("Error saving Invenotry items list to file");
 //}   finally {
 //    if (outFile != null) {
@@ -42,8 +61,8 @@
 //    }
 //  }
 //}
-//
-//
+
+
 //try (PrintWriter out = new PrintWriter(filePath)) {
 //
 //   out.println("Your report text goes here");
@@ -55,5 +74,7 @@
 //} catch (IOException ex) {
 //ErrorView.display(this.getClass().getName(), "I/0 Error: " + ex.getMessage());
 //}
-//
-//
+
+
+
+}
